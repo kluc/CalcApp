d91812a81288095526a49115529d75126a108ce6
kluc <Kelson@chandraanthsmbp.na.xerox.net>
2015-12-28 17:07:50 -0800

    changed something

--- app/src/main/java/com/test/kelson/calculator/MainActivity.java
+++ app/src/main/java/com/test/kelson/calculator/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
     Calculator calc = new Calculator();
 
 
-    @Overridegi
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
